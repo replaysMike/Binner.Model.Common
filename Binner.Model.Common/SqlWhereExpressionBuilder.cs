4571178c142c6dd62ad82a5b16f34b888b235fbf
Michael Brown <mikeb@360replays.com>
2022-01-10 14:43:59 -0800

    moved sql expression builder namespace

--- Binner.Model.Common/SqlWhereExpressionBuilder.cs
+++ Binner.Model.Common/SqlWhereExpressionBuilder.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using TypeSupport.Extensions;
 
-namespace Binner.Common.StorageProviders
+namespace Binner.Model.Common
 {
     /// <summary>
     /// Builds SQL Where condition based on an expression tree
@@ -247,75 +247,4 @@ namespace Binner.Common.StorageProviders
             }
         }
     }
-
-    public class ParseException : Exception
-    {
-        public ParseException(string message) : base(message) { }
-    }
-
-    public class WhereCondition
-    {
-        public string? Sql { get; set; }
-        public Dictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
-
-        public static WhereCondition IsSql(string? sql)
-        {
-            return new WhereCondition()
-            {
-                Parameters = new Dictionary<string, object>(),
-                Sql = sql
-            };
-        }
-
-        public static WhereCondition IsParameter(int count, object value)
-        {
-            return new WhereCondition()
-            {
-                Parameters = { { count.ToString(), value } },
-                Sql = $"@{count}"
-            };
-        }
-
-        public static WhereCondition IsCollection(ref int countStart, IEnumerable values)
-        {
-            var parameters = new Dictionary<string, object>();
-            var sql = new StringBuilder("(");
-            foreach (var value in values)
-            {
-                parameters.Add((countStart).ToString(), value);
-                sql.Append($"@{countStart},");
-                countStart++;
-            }
-            if (sql.Length == 1)
-            {
-                sql.Append("null,");
-            }
-            sql[sql.Length - 1] = ')';
-            return new WhereCondition()
-            {
-                Parameters = parameters,
-                Sql = sql.ToString()
-            };
-        }
-
-        public static WhereCondition Concat(string @operator, WhereCondition operand)
-        {
-            return new WhereCondition()
-            {
-                Parameters = operand.Parameters,
-                Sql = $"({@operator} {operand.Sql})"
-            };
-        }
-
-        public static WhereCondition Concat(WhereCondition left, string @operator, WhereCondition right)
-        {
-            return new WhereCondition()
-            {
-                Parameters = left.Parameters.Union(right.Parameters).ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
-                Sql = $"({left.Sql} {@operator} {right.Sql})"
-            };
-        }
-    }
-
-
 }
