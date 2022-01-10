cebaea03cb2ec2a1c5f8522138d6338ca8fe10d2
Michael Brown <mikeb@360replays.com>
2022-01-10 14:02:08 -0800

    adds additional Binner types

--- Binner.Model.Common/SystemDefaults.cs
+++ Binner.Model.Common/SystemDefaults.cs
@@ -9,5 +9,62 @@
         /// The part should be reordered when it gets below this value
         /// </summary>
         public const int LowStockThreshold = 1;
+
+        /// <summary>
+        /// Default part type definitions
+        /// </summary>
+        public enum DefaultPartTypes
+        {
+            Resistor = 1,
+            Capacitor,
+            Inductor,
+            Diode,
+            LED,
+            Transistor,
+            Relay,
+            Transformer,
+            Crystal,
+            Sensor,
+            Switch,
+            Cable,
+            Connector,
+            IC,
+            Hardware,
+            Other,
+            [Parent(DefaultPartTypes.IC)]
+            OpAmp,
+            [Parent(DefaultPartTypes.IC)]
+            Amplifier,
+            [Parent(DefaultPartTypes.IC)]
+            Memory,
+            [Parent(DefaultPartTypes.IC)]
+            Logic,
+            [Parent(DefaultPartTypes.IC)]
+            Interface,
+            [Parent(DefaultPartTypes.IC)]
+            Microcontroller,
+            [Parent(DefaultPartTypes.IC)]
+            Clock,
+            [Parent(DefaultPartTypes.IC)]
+            ADC,
+            [Parent(DefaultPartTypes.IC)]
+            VoltageRegulator,
+            [Parent(DefaultPartTypes.IC)]
+            EnergyMetering,
+            [Parent(DefaultPartTypes.IC)]
+            LedDriver,
+            [Parent(DefaultPartTypes.Transistor)]
+            MOSFET,
+            [Parent(DefaultPartTypes.Transistor)]
+            IGBT,
+            [Parent(DefaultPartTypes.Transistor)]
+            JFET,
+            [Parent(DefaultPartTypes.Transistor)]
+            SCR,
+            [Parent(DefaultPartTypes.Transistor)]
+            DIAC,
+            [Parent(DefaultPartTypes.Transistor)]
+            TRIAC,
+        }
     }
 }
