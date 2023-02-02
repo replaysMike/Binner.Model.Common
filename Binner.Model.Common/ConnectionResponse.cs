11a62d247dc0dd74b1ddb62a7728afa4830f6c2f
Michael Brown <mikeb@360replays.com>
2023-02-02 11:36:57 -0800

    add a check if database exists

--- Binner.Model.Common/ConnectionResponse.cs
+++ Binner.Model.Common/ConnectionResponse.cs
@@ -3,6 +3,7 @@
     public class ConnectionResponse
     {
         public bool IsSuccess { get; set; }
-        public ICollection<string> Errors { get; set; }
+        public bool DatabaseExists { get; set; }
+        public ICollection<string> Errors { get; set; } = new List<string>();
     }
 }
